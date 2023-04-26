a1f3847bd5b1153b8ad9246dd62037308bce39c6
Cengiz <Cengiz@gmail.com>
2023-04-26 13:31:14 +0300

    Sekme ekleme-kaldırma ve belgeyi kaydetme-belgeyi açma işlemleri için fonksiyonlar yazıldı.

--- NoteMaster/Note.cs
+++ NoteMaster/Note.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
@@ -19,50 +20,111 @@ namespace NoteMaster
         {
             InitializeComponent();
         }
-        #region Fonksiyonlar
-        #region TümSekmeler
+       
 
         private void sekmeEkle()
         {
+            // Yeni bir RichTextBox nesnesi oluşturmak
+            RichTextBox newRichTextBox = new RichTextBox();
+            newRichTextBox.Name = "Body";
+            newRichTextBox.Dock = DockStyle.Fill;
+            newRichTextBox.ContextMenuStrip = contextMenuStrip1;
 
+            // Yeni bir TabPage nesnesi oluşturmak
+            TabPage newTabPage = new TabPage();
+            sekmeSayısı += 1;
+
+            string documentText = "Belge" + sekmeSayısı;
+            newTabPage.Name = "DocumentText";
+            newTabPage.Text = documentText;
+
+            // TabPage nesnesinin Controls koleksiyonuna RichTextBox nesnesi eklemek
+            newTabPage.Controls.Add(newRichTextBox);
+
+            // TabControl nesnesine yeni sekme eklemek
+            tabControl1.TabPages.Add(newTabPage);
         }
         private void sekmeKapat()
         {
-
+           // Eğer mevcut sekmeden fazla sekme varsa
+            if (tabControl1.TabPages.Count > 1)
+            {
+                tabControl1.TabPages.Remove(tabControl1.SelectedTab);  // Mevcut seçili sekme kaldırılır
+            }
+            else
+            {
+                tabControl1.TabPages.Add(tabControl1.SelectedTab);
+                sekmeEkle(); // En az 1 sekme olmalı
+            }
         }
+
         private void tümSekmeleriKapat()
         {
-
+            foreach(TabPage page in tabControl1.TabPages)
+            {
+                tabControl1.TabPages.Remove(page);
+            }
+            sekmeEkle();
         }
         private void hariciSekmeleriKapat()
         {
-
+            foreach (TabPage page in tabControl1.TabPages)
+            {
+                if(page.Name != tabControl1.SelectedTab.Name)
+                {
+                    tabControl1.TabPages.Remove(page);
+                }
+            }
         }
-        #endregion
-        #region KaydetveAç
+       
         private void kaydet()
         {
+            saveFileDialog1.FileName = tabControl1.SelectedTab.Name;
+            saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            saveFileDialog1.Filter = "RTF | *.rtf";
+            saveFileDialog1.Title = "Kaydet";
 
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (saveFileDialog1.FileName.Length > 0)
+                {
+                    getCurrentDocument.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.RichText);
+                }
+            }
         }
         private void farklıKaydet()
         {
-
+            saveFileDialog1.FileName = tabControl1.SelectedTab.Name;
+            saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            saveFileDialog1.Filter = "Text dosya | *.txt | Tüm Dosyalar|*.*";
+            saveFileDialog1.Title = "Farklı Kaydet";
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (saveFileDialog1.FileName.Length > 0)
+                {
+                    getCurrentDocument.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+                }
+            }
         }
         private void belgeAç()
         {
-
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog1.Filter = "RTF |*.rtf|Text dosya|*.txt|Tüm Dosyalar|*.*";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (openFileDialog1.FileName.Length > 0)
+                {
+                    getCurrentDocument.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+                }
+            }
         }
 
-
-        #endregion
-        #region Özellikler
-        private RichTextBox getCurrentDocument 
+        private RichTextBox getCurrentDocument // Aktif belgeyi almak
         {
             get { return (RichTextBox)tabControl1.SelectedTab.Controls["Body"]; }
         }
         
-        #endregion
-        #endregion
+        
         private void Form1_Load(object sender, EventArgs e)
         {
            
@@ -80,7 +142,7 @@ namespace NoteMaster
 
         private void kaydetToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+           
         }
     }
 }
